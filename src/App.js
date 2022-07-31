8ebea2e9ace7e404060c01e3a0a7e863b363f50d
shyamal <shyamalpatil2003@gmail.com>
2022-07-31 17:17:04 +0530

    s

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import classes from "./App.module.css";
+function App() {
+  return (
+    <div className={`${classes.div}`}>
+      <h1>Sagar Patil </h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./Header.module.css";
+const Header = () => {
+  return <div className={`${classes.div}`}>Header</div>;
+};
+
+export default Header;
